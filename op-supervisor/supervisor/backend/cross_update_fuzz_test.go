b2a9d1033e9e95b8cbf170ac68d269aecafb3cd0
lisandrasilva <lisandrasilva18@hotmail.com>
2025-06-11 13:04:19 +0100

    Defined Local-unsafe and cross-unsafe heads

--- op-supervisor/supervisor/backend/cross_update_fuzz_test.go
+++ op-supervisor/supervisor/backend/cross_update_fuzz_test.go
@@ -29,100 +29,113 @@ import (
 
 func FuzzCrossUpdate(f *testing.F) {
 
-	f.Add(uint(1), uint(2))
+	logger := testlog.Logger(f, log.LvlInfo)
+	m := metrics.NoopMetrics
+	dataDir := f.TempDir()
+	chainA := eth.ChainIDFromUInt64(900)
+	chainB := eth.ChainIDFromUInt64(901)
+	depSet, err := depset.NewStaticConfigDependencySet(
+		map[eth.ChainID]*depset.StaticConfigDependency{
+			chainA: {
+				ChainIndex:     900,
+				ActivationTime: 42,
+				HistoryMinTime: 100,
+			},
+			chainB: {
+				ChainIndex:     901,
+				ActivationTime: 30,
+				HistoryMinTime: 20,
+			},
+		})
+	require.NoError(f, err)
+	cfg := &config.Config{
+		Version:               "test",
+		LogConfig:             oplog.CLIConfig{},
+		MetricsConfig:         opmetrics.CLIConfig{},
+		PprofConfig:           oppprof.CLIConfig{},
+		RPC:                   oprpc.CLIConfig{},
+		DependencySetSource:   depSet,
+		SynchronousProcessors: true,
+		MockRun:               false,
+		SyncSources:           &syncnode.CLISyncNodes{},
+		Datadir:               dataDir,
+	}
+
+	ex := event.NewGlobalSynchronous(context.Background())
+	b, err := NewSupervisorBackend(context.Background(), logger, m, cfg, ex)
+	require.NoError(f, err)
+	f.Log("initialized!")
+
+	l1Src := &testutils.MockL1Source{}
+	b.AttachL1Source(l1Src)
+
+	srcChainA := &MockProcessorSource{}
+	require.NoError(f, b.AttachProcessorSource(chainA, srcChainA))
+
+	srcChainB := &MockProcessorSource{}
+	require.NoError(f, b.AttachProcessorSource(chainB, srcChainB))
+
+	err = b.Start(context.Background())
+	require.NoError(f, err)
+	f.Log("started!")
+
+	f.Add(uint(4), uint(1))
 
 	f.Fuzz(func(t *testing.T, chainALength uint, chainBLength uint) {
 
-		logger := testlog.Logger(t, log.LvlInfo)
-		m := metrics.NoopMetrics
-		dataDir := t.TempDir()
-		chainA := eth.ChainIDFromUInt64(900)
-		chainB := eth.ChainIDFromUInt64(901)
-		depSet, err := depset.NewStaticConfigDependencySet(
-			map[eth.ChainID]*depset.StaticConfigDependency{
-				chainA: {
-					ChainIndex:     900,
-					ActivationTime: 42,
-					HistoryMinTime: 100,
-				},
-				chainB: {
-					ChainIndex:     901,
-					ActivationTime: 30,
-					HistoryMinTime: 20,
-				},
-			})
-		require.NoError(t, err)
-		cfg := &config.Config{
-			Version:               "test",
-			LogConfig:             oplog.CLIConfig{},
-			MetricsConfig:         opmetrics.CLIConfig{},
-			PprofConfig:           oppprof.CLIConfig{},
-			RPC:                   oprpc.CLIConfig{},
-			DependencySetSource:   depSet,
-			SynchronousProcessors: true,
-			MockRun:               false,
-			SyncSources:           &syncnode.CLISyncNodes{},
-			Datadir:               dataDir,
-		}
-
-		ex := event.NewGlobalSynchronous(context.Background())
-		b, err := NewSupervisorBackend(context.Background(), logger, m, cfg, ex)
-		require.NoError(t, err)
-		t.Log("initialized!")
-
-		l1Src := &testutils.MockL1Source{}
-		b.AttachL1Source(l1Src)
-
-		srcChainA := &MockProcessorSource{}
-		require.NoError(t, b.AttachProcessorSource(chainA, srcChainA))
-
 		t.Run("ChainA Initialization", func(t *testing.T) {
 			//t.Parallel()
-			t.Logf("Chain A length: %d", chainALength%10)
+			chainALength = chainALength % 10
+			t.Logf("Chain A length: %d", chainALength)
+
+			var block eth.BlockRef
 
 			if chainALength > 0 {
 				// Initialize the chainA source with a genesis block
-				genesisBlock := eth.BlockRef{
+				block = eth.BlockRef{
 					Hash:       common.BytesToHash([]byte{0xaa, 0x00}),
 					Number:     0,
 					ParentHash: common.Hash{}, // genesis has no parent hash
 					Time:       uint64(time.Now().Unix()),
 				}
-				t.Logf("Chain A genesis block:%s", genesisBlock.Hash.Hex())
+				t.Logf("Chain A genesis block:%s", block.Hash.Hex())
 
-				srcChainA.ExpectBlockRefByNumber(0, genesisBlock, nil)
-				srcChainA.ExpectFetchReceipts(genesisBlock.Hash, nil, nil)
+				srcChainA.ExpectBlockRefByNumber(0, block, nil)
+				srcChainA.ExpectFetchReceipts(block.Hash, nil, nil)
 				// Emit the anchor event for chain A with the genesis block
 				// This is necessary to initialize the database with the genesis block
 				b.emitter.Emit(superevents.AnchorEvent{
 					ChainID: chainA,
 					Anchor: types.DerivedBlockRefPair{
-						Derived: genesisBlock,
+						Derived: block,
 						Source:  eth.L1BlockRef{},
 					}})
+
+				i := 1
+				for ; i < int(chainALength); i++ {
+					block = eth.BlockRef{
+						Hash:       common.BytesToHash([]byte{0xaa, byte(i)}),
+						Number:     uint64(i),
+						ParentHash: common.BytesToHash([]byte{0xaa, byte(i - 1)}),
+						Time:       uint64(time.Now().Add(time.Duration(i*10) * time.Minute).Unix()),
+					}
+					t.Logf("Chain A block %d: %s\t Timestamp:%d", i, block.Hash.Hex(), block.Time)
+					// Expect the source to return the block by number
+					srcChainA.ExpectBlockRefByNumber(uint64(i), block, nil)
+					srcChainA.ExpectFetchReceipts(block.Hash, nil, nil)
+				}
+				srcChainA.ExpectBlockRefByNumber(uint64(i), eth.L1BlockRef{}, ethereum.NotFound)
+
+				b.emitter.Emit(superevents.LocalUnsafeReceivedEvent{
+					ChainID:        chainA,
+					NewLocalUnsafe: block,
+				})
+				t.Log("Emitted LocalUnsafeReceivedEvent for Chain A")
 			} else {
 				t.Log("Chain A has no blocks to initialize")
 			}
-			i := 1
-			for ; i < int(chainALength); i++ {
-				block := eth.BlockRef{
-					Hash:       common.BytesToHash([]byte{0xaa, byte(i)}),
-					Number:     uint64(i),
-					ParentHash: common.Hash{0xaa, byte(i - 1)},
-					Time:       uint64(time.Now().Add(time.Duration(i*10) * time.Minute).Unix()),
-				}
-				t.Logf("Chain A block %d: %s\t Timestamp:%d", i, block.Hash.Hex(), block.Time)
-				// Expect the source to return the block by number
-				srcChainA.ExpectBlockRefByNumber(uint64(i), block, nil)
-				srcChainA.ExpectFetchReceipts(block.Hash, nil, nil)
-			}
-
-			srcChainA.ExpectBlockRefByNumber(uint64(i), eth.L1BlockRef{}, ethereum.NotFound)
 
 		})
-
-		srcChainB := &MockProcessorSource{}
-		require.NoError(t, b.AttachProcessorSource(chainB, srcChainB))
 
 		t.Run("ChainB Initialization", func(t *testing.T) {
 			//t.Parallel()
@@ -165,18 +178,29 @@ func FuzzCrossUpdate(f *testing.F) {
 			}
 			srcChainB.ExpectBlockRefByNumber(uint64(i), eth.L1BlockRef{}, ethereum.NotFound)
 		})
-
-		err = b.Start(context.Background())
-		require.NoError(t, err)
-		t.Log("started!")
-
-		// After the anchor event, the database is initialized, and the call to update
-		// from the LocalUnsafe event will succeed.
-
-		require.NoError(t, ex.Drain())
-
-		err = b.Stop(context.Background())
-		require.NoError(t, err)
-		t.Log("stopped!")
 	})
+
+	// After the anchor event, the database is initialized, and the call to update
+	// from the LocalUnsafe event will succeed.
+
+	//require.NoError(f, ex.DrainUntil(
+	//	func(ev event.Event) bool {
+	//		return ev == superevents.UpdateCrossUnsafeRequestEvent{
+	//			ChainID: chainA,
+	//		}
+	//	},
+	//	true))
+	require.NoError(f, ex.Drain())
+
+	localUnsafe, err := b.LocalUnsafe(context.Background(), chainA)
+	require.NoError(f, err)
+	f.Logf("Local Unsafe head for Chain A: %d", localUnsafe.Number)
+
+	crossUnsafe, err := b.CrossUnsafe(context.Background(), chainA)
+	require.NoError(f, err)
+	f.Logf("Cross Unsafe head for Chain A: %d", crossUnsafe.Number)
+
+	err = b.Stop(context.Background())
+	require.NoError(f, err)
+	f.Log("stopped!")
 }
