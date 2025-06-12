fa3b23538133cff5e5f75d8250362d54de212553
lisandrasilva <lisandrasilva18@hotmail.com>
2025-06-12 09:58:30 +0100

    Inital local-unsafe and cross-unsafe head for chain A

--- op-supervisor/supervisor/backend/cross_update_fuzz_test.go
+++ op-supervisor/supervisor/backend/cross_update_fuzz_test.go
@@ -29,178 +29,182 @@ import (
 
 func FuzzCrossUpdate(f *testing.F) {
 
-	logger := testlog.Logger(f, log.LvlInfo)
-	m := metrics.NoopMetrics
-	dataDir := f.TempDir()
-	chainA := eth.ChainIDFromUInt64(900)
-	chainB := eth.ChainIDFromUInt64(901)
-	depSet, err := depset.NewStaticConfigDependencySet(
-		map[eth.ChainID]*depset.StaticConfigDependency{
-			chainA: {
-				ChainIndex:     900,
-				ActivationTime: 42,
-				HistoryMinTime: 100,
-			},
-			chainB: {
-				ChainIndex:     901,
-				ActivationTime: 30,
-				HistoryMinTime: 20,
-			},
-		})
-	require.NoError(f, err)
-	cfg := &config.Config{
-		Version:               "test",
-		LogConfig:             oplog.CLIConfig{},
-		MetricsConfig:         opmetrics.CLIConfig{},
-		PprofConfig:           oppprof.CLIConfig{},
-		RPC:                   oprpc.CLIConfig{},
-		DependencySetSource:   depSet,
-		SynchronousProcessors: true,
-		MockRun:               false,
-		SyncSources:           &syncnode.CLISyncNodes{},
-		Datadir:               dataDir,
-	}
+	f.Add(uint(4), uint(2), uint(2)) // Add initial values for fuzzing
 
-	ex := event.NewGlobalSynchronous(context.Background())
-	b, err := NewSupervisorBackend(context.Background(), logger, m, cfg, ex)
-	require.NoError(f, err)
-	f.Log("initialized!")
+	f.Fuzz(func(t *testing.T, chainALength uint, chainBLength uint, crossUnsafeHeadIndex uint) {
+		logger := testlog.Logger(t, log.LvlInfo)
+		m := metrics.NoopMetrics
+		dataDir := t.TempDir()
+		chainA := eth.ChainIDFromUInt64(900)
+		chainB := eth.ChainIDFromUInt64(901)
+		depSet, err := depset.NewStaticConfigDependencySet(
+			map[eth.ChainID]*depset.StaticConfigDependency{
+				chainA: {
+					ChainIndex:     900,
+					ActivationTime: 42,
+					HistoryMinTime: 100,
+				},
+				chainB: {
+					ChainIndex:     901,
+					ActivationTime: 30,
+					HistoryMinTime: 20,
+				},
+			})
+		require.NoError(t, err)
+		cfg := &config.Config{
+			Version:               "test",
+			LogConfig:             oplog.CLIConfig{},
+			MetricsConfig:         opmetrics.CLIConfig{},
+			PprofConfig:           oppprof.CLIConfig{},
+			RPC:                   oprpc.CLIConfig{},
+			DependencySetSource:   depSet,
+			SynchronousProcessors: true,
+			MockRun:               false,
+			SyncSources:           &syncnode.CLISyncNodes{},
+			Datadir:               dataDir,
+		}
 
-	l1Src := &testutils.MockL1Source{}
-	b.AttachL1Source(l1Src)
+		ex := event.NewGlobalSynchronous(context.Background())
+		b, err := NewSupervisorBackend(context.Background(), logger, m, cfg, ex)
+		require.NoError(t, err)
+		t.Log("initialized!")
 
-	srcChainA := &MockProcessorSource{}
-	require.NoError(f, b.AttachProcessorSource(chainA, srcChainA))
+		l1Src := &testutils.MockL1Source{}
+		b.AttachL1Source(l1Src)
 
-	srcChainB := &MockProcessorSource{}
-	require.NoError(f, b.AttachProcessorSource(chainB, srcChainB))
+		srcChainA := &MockProcessorSource{}
+		require.NoError(t, b.AttachProcessorSource(chainA, srcChainA))
 
-	err = b.Start(context.Background())
-	require.NoError(f, err)
-	f.Log("started!")
+		srcChainB := &MockProcessorSource{}
+		require.NoError(t, b.AttachProcessorSource(chainB, srcChainB))
 
-	f.Add(uint(4), uint(1))
+		err = b.Start(context.Background())
+		require.NoError(t, err)
+		t.Log("started!")
 
-	f.Fuzz(func(t *testing.T, chainALength uint, chainBLength uint) {
+		chainALength = chainALength%10 + 1
+		chainBLength = chainBLength % 10
+		crossUnsafeHeadIndex = crossUnsafeHeadIndex % (chainALength + 1)
+		t.Logf("Fuzzing with Chain A length: %d, Chain B length: %d, Cross Unsafe Head Index: %d", chainALength, chainBLength, crossUnsafeHeadIndex)
 
-		t.Run("ChainA Initialization", func(t *testing.T) {
-			//t.Parallel()
-			chainALength = chainALength % 10
-			t.Logf("Chain A length: %d", chainALength)
+		crossUnsafeHead := ChainAInit(t, b, chainA, srcChainA, chainALength, crossUnsafeHeadIndex)
+		ChainBInit(t, b, chainB, srcChainB, chainBLength)
 
-			var block eth.BlockRef
+		require.NoError(t, ex.Drain())
+		err = b.chainDBs.UpdateCrossUnsafe(chainA, crossUnsafeHead)
+		require.NoError(t, err)
 
-			if chainALength > 0 {
-				// Initialize the chainA source with a genesis block
-				block = eth.BlockRef{
-					Hash:       common.BytesToHash([]byte{0xaa, 0x00}),
-					Number:     0,
-					ParentHash: common.Hash{}, // genesis has no parent hash
-					Time:       uint64(time.Now().Unix()),
-				}
-				t.Logf("Chain A genesis block:%s", block.Hash.Hex())
+		localUnsafe, err := b.LocalUnsafe(context.Background(), chainA)
+		require.NoError(t, err)
+		t.Logf("Local Unsafe head for Chain A: %d", localUnsafe.Number)
 
-				srcChainA.ExpectBlockRefByNumber(0, block, nil)
-				srcChainA.ExpectFetchReceipts(block.Hash, nil, nil)
-				// Emit the anchor event for chain A with the genesis block
-				// This is necessary to initialize the database with the genesis block
-				b.emitter.Emit(superevents.AnchorEvent{
-					ChainID: chainA,
-					Anchor: types.DerivedBlockRefPair{
-						Derived: block,
-						Source:  eth.L1BlockRef{},
-					}})
+		crossUnsafe, err := b.CrossUnsafe(context.Background(), chainA)
+		require.NoError(t, err)
+		t.Logf("Cross Unsafe head for Chain A: %d", crossUnsafe.Number)
 
-				i := 1
-				for ; i < int(chainALength); i++ {
-					block = eth.BlockRef{
-						Hash:       common.BytesToHash([]byte{0xaa, byte(i)}),
-						Number:     uint64(i),
-						ParentHash: common.BytesToHash([]byte{0xaa, byte(i - 1)}),
-						Time:       uint64(time.Now().Add(time.Duration(i*10) * time.Minute).Unix()),
-					}
-					t.Logf("Chain A block %d: %s\t Timestamp:%d", i, block.Hash.Hex(), block.Time)
-					// Expect the source to return the block by number
-					srcChainA.ExpectBlockRefByNumber(uint64(i), block, nil)
-					srcChainA.ExpectFetchReceipts(block.Hash, nil, nil)
-				}
-				srcChainA.ExpectBlockRefByNumber(uint64(i), eth.L1BlockRef{}, ethereum.NotFound)
-
-				b.emitter.Emit(superevents.LocalUnsafeReceivedEvent{
-					ChainID:        chainA,
-					NewLocalUnsafe: block,
-				})
-				t.Log("Emitted LocalUnsafeReceivedEvent for Chain A")
-			} else {
-				t.Log("Chain A has no blocks to initialize")
-			}
-
-		})
-
-		t.Run("ChainB Initialization", func(t *testing.T) {
-			//t.Parallel()
-			t.Logf("Chain B length: %d", chainBLength%10)
-			if chainBLength > 0 {
-				// Initialize the chainB source with a genesis block
-				genesisBlock := eth.BlockRef{
-					Hash:       common.BytesToHash([]byte{0xbb, 0x00}),
-					Number:     0,
-					ParentHash: common.Hash{}, // genesis has no parent hash
-					Time:       uint64(time.Now().Unix()),
-				}
-				t.Logf("Chain B genesis block: %s", genesisBlock.Hash.Hex())
-
-				srcChainB.ExpectBlockRefByNumber(0, genesisBlock, nil)
-				srcChainB.ExpectFetchReceipts(genesisBlock.Hash, nil, nil)
-				// Emit the anchor event for chain B
-				// This is necessary to initialize the database with the genesis block
-				b.emitter.Emit(superevents.AnchorEvent{
-					ChainID: chainA,
-					Anchor: types.DerivedBlockRefPair{
-						Derived: genesisBlock,
-						Source:  eth.L1BlockRef{},
-					}})
-			} else {
-				t.Log("Chain B has no blocks to initialize")
-			}
-			i := 1
-			for ; i < int(chainBLength); i++ {
-				block := eth.BlockRef{
-					Hash:       common.BytesToHash([]byte{0xbb, byte(i)}),
-					Number:     uint64(i),
-					ParentHash: common.Hash{0xbb, byte(i - 1)},
-					Time:       uint64(time.Now().Add(time.Duration(i*10) * time.Minute).Unix()),
-				}
-				t.Logf("Chain B block %d: %s\t Timestamp:%d", i, block.Hash.Hex(), block.Time)
-				// Expect the source to return the block by number
-				srcChainB.ExpectBlockRefByNumber(uint64(i), block, nil)
-				srcChainB.ExpectFetchReceipts(block.Hash, nil, nil)
-			}
-			srcChainB.ExpectBlockRefByNumber(uint64(i), eth.L1BlockRef{}, ethereum.NotFound)
-		})
+		err = b.Stop(context.Background())
+		require.NoError(t, err)
+		t.Log("stopped!")
 	})
 
-	// After the anchor event, the database is initialized, and the call to update
-	// from the LocalUnsafe event will succeed.
+}
 
-	//require.NoError(f, ex.DrainUntil(
-	//	func(ev event.Event) bool {
-	//		return ev == superevents.UpdateCrossUnsafeRequestEvent{
-	//			ChainID: chainA,
-	//		}
-	//	},
-	//	true))
-	require.NoError(f, ex.Drain())
+func ChainAInit(t *testing.T, b *SupervisorBackend, chainA eth.ChainID, srcChainA *MockProcessorSource, chainALength uint, crossUnsafeHeadIndex uint) types.BlockSeal {
+	t.Log("Initializing Chain A")
+	t.Logf("Chain A length: %d", chainALength)
 
-	localUnsafe, err := b.LocalUnsafe(context.Background(), chainA)
-	require.NoError(f, err)
-	f.Logf("Local Unsafe head for Chain A: %d", localUnsafe.Number)
+	var block, crossUnsafeHead eth.BlockRef
 
-	crossUnsafe, err := b.CrossUnsafe(context.Background(), chainA)
-	require.NoError(f, err)
-	f.Logf("Cross Unsafe head for Chain A: %d", crossUnsafe.Number)
+	if chainALength > 0 {
+		// Initialize the chainA source with a genesis block
+		block = eth.BlockRef{
+			Hash:       common.BytesToHash([]byte{0xaa, 0x00}),
+			Number:     0,
+			ParentHash: common.Hash{}, // genesis has no parent hash
+			Time:       uint64(time.Now().Unix()),
+		}
+		crossUnsafeHead = block
+		t.Logf("Chain A genesis block:%s", block.Hash.Hex())
+		srcChainA.ExpectBlockRefByNumber(0, block, nil)
+		srcChainA.ExpectFetchReceipts(block.Hash, nil, nil)
+		// Emit the anchor event for chain A with the genesis block
+		// This is necessary to initialize the database with the genesis block
+		b.emitter.Emit(superevents.AnchorEvent{
+			ChainID: chainA,
+			Anchor: types.DerivedBlockRefPair{
+				Derived: block,
+				Source:  eth.L1BlockRef{},
+			}})
+		i := 1
+		for ; i < int(chainALength); i++ {
+			block = eth.BlockRef{
+				Hash:       common.BytesToHash([]byte{0xaa, byte(i)}),
+				Number:     uint64(i),
+				ParentHash: common.BytesToHash([]byte{0xaa, byte(i - 1)}),
+				Time:       uint64(time.Now().Add(time.Duration(i*5) * time.Minute).Unix()),
+			}
+			if i == int(crossUnsafeHeadIndex) {
+				// Set the cross unsafe head to a specific block
+				crossUnsafeHead = block
+			}
+			t.Logf("Chain A block %d: %s\t Timestamp:%d", i, block.Hash.Hex(), block.Time)
+			// Expect the source to return the block by number
+			srcChainA.ExpectBlockRefByNumber(uint64(i), block, nil)
+			srcChainA.ExpectFetchReceipts(block.Hash, nil, nil)
+		}
+		srcChainA.ExpectBlockRefByNumber(uint64(i), eth.L1BlockRef{}, ethereum.NotFound)
+		// After the anchor event, the database is initialized, and the call to update
+		// from the LocalUnsafe event will succeed.
+		b.emitter.Emit(superevents.LocalUnsafeReceivedEvent{
+			ChainID:        chainA,
+			NewLocalUnsafe: block,
+		})
+		t.Log("Emitted LocalUnsafeReceivedEvent for Chain A")
+		return types.BlockSealFromRef(crossUnsafeHead)
+	} else {
+		t.Log("Chain A has no blocks to initialize")
+		return types.BlockSeal{}
+	}
+}
 
-	err = b.Stop(context.Background())
-	require.NoError(f, err)
-	f.Log("stopped!")
+func ChainBInit(t *testing.T, b *SupervisorBackend, chainB eth.ChainID, srcChainB *MockProcessorSource, chainBLength uint) {
+	t.Log("Initializing Chain B")
+	t.Logf("Chain B length: %d", chainBLength)
+
+	if chainBLength > 0 {
+		// Initialize the chainB source with a genesis block
+		genesisBlock := eth.BlockRef{
+			Hash:       common.BytesToHash([]byte{0xbb, 0x00}),
+			Number:     0,
+			ParentHash: common.Hash{}, // genesis has no parent hash
+			Time:       uint64(time.Now().Unix()),
+		}
+		t.Logf("Chain B genesis block: %s", genesisBlock.Hash.Hex())
+
+		srcChainB.ExpectBlockRefByNumber(0, genesisBlock, nil)
+		srcChainB.ExpectFetchReceipts(genesisBlock.Hash, nil, nil)
+		// Emit the anchor event for chain B
+		b.emitter.Emit(superevents.AnchorEvent{
+			ChainID: chainB,
+			Anchor: types.DerivedBlockRefPair{
+				Derived: genesisBlock,
+				Source:  eth.L1BlockRef{},
+			}})
+		i := 1
+		for ; i < int(chainBLength); i++ {
+			block := eth.BlockRef{
+				Hash:       common.BytesToHash([]byte{0xbb, byte(i)}),
+				Number:     uint64(i),
+				ParentHash: common.Hash{0xbb, byte(i - 1)},
+				Time:       uint64(time.Now().Add(time.Duration(i*10) * time.Minute).Unix()),
+			}
+			t.Logf("Chain B block %d: %s\t Timestamp:%d", i, block.Hash.Hex(), block.Time)
+			// Expect the source to return the block by number
+			srcChainB.ExpectBlockRefByNumber(uint64(i), block, nil)
+			srcChainB.ExpectFetchReceipts(block.Hash, nil, nil)
+		}
+		srcChainB.ExpectBlockRefByNumber(uint64(i), eth.L1BlockRef{}, ethereum.NotFound)
+	} else {
+		t.Log("Chain B has no blocks to initialize")
+	}
 }
